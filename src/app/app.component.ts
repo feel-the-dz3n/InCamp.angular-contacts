4fdd12a982e45b14098b258718b20f2665ec40f9
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-14 22:05:52 +0200

    Selectable contacts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  selectedContact: any;
   title = 'Some cool app';
 
   contacts = [
     'Yaroslav', 'Alex', 'Ruslan'
   ]
+
+  selectContact(contact: any) {
+    this.selectedContact = contact;
+  }
 }
