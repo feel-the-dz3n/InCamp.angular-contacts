388c3fb9ef9bf70f5d7860675780cb15cc996cb0
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-15 09:34:44 +0200

    Move contact details to a module, make styles global

---
+++ src/app/contact/contact-detail/contact-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-detail',
+  templateUrl: './contact-detail.component.html',
+  styleUrls: ['./contact-detail.component.scss']
+})
+export class ContactDetailComponent {
+  @Input() contact: any;
+}
